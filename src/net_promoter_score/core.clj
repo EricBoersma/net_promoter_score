e88d34e47ff0c55d053eb001f420c0efaf374bf2
Eric Boersma <eric.boersma@gmail.com>
2019-01-03 10:23:44 -0500

    Initial project structure

---
+++ src/net_promoter_score/core.clj
@@ -0,0 +1,6 @@
+(ns net-promoter-score.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/net_promoter_score/core_test.clj
@@ -0,0 +1,7 @@
+(ns net-promoter-score.core-test
+  (:require [clojure.test :refer :all]
+            [net-promoter-score.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
