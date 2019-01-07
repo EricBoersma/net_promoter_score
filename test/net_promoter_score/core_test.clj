f7832ebd4d0d43d7ecb76d5dde33762151263c82
Eric Boersma <eric.boersma@gmail.com>
2019-01-07 11:14:23 -0500

    Cleaning up some code in the core_test

--- test/net_promoter_score/core_test.clj
+++ test/net_promoter_score/core_test.clj
@@ -4,25 +4,25 @@
 
 (defn get-random-detractor-values
   "Gets a list of the specified number of detractor values (less than 7)"
-  [count]
-  (take count (repeatedly #(rand-int 7))))
+  [count source]
+  (into source (take count (repeatedly #(rand-int 7)))))
 
 (defn get-random-passive-values
   "Gets a list of the specified number of passive values (7 and 8)"
-  [count]
-  (take count (repeatedly #(+ (rand-int 2) 7))))
+  [count source]
+  (into source (take count (repeatedly #(+ (rand-int 2) 7)))))
 
 (defn get-random-promoter-values
   "Gets a list of the specified number of promoter values (9 and 10)"
-  [count]
-  (take count (repeatedly #(+ (rand-int 2) 9))))
+  [count source]
+  (into source (take count (repeatedly #(+ (rand-int 2) 9)))))
 
 (deftest get-promoter-score-test
   (testing "Correctly calculates the NPS based on some large number of values"
-           (is (= 0 (get-promoter-score (flatten (conj (get-random-detractor-values 10) (get-random-passive-values 10) (get-random-promoter-values 10))))))
-           (is (= 66 (get-promoter-score (flatten (conj (get-random-detractor-values 10) (get-random-passive-values 10) (get-random-promoter-values 70))))))
-           (is (= 70 (get-promoter-score (flatten (conj (get-random-detractor-values 100) (get-random-passive-values 100) (get-random-promoter-values 800))))))
-           (is (= -70 (get-promoter-score (flatten (conj (get-random-detractor-values 8000) (get-random-passive-values 1000) (get-random-promoter-values 1000))))))
+           (is (= 0 (get-promoter-score (->> [] (get-random-detractor-values 10) (get-random-passive-values 10) (get-random-promoter-values 10)))))
+           (is (= 66 (get-promoter-score (->> [] (get-random-detractor-values 10) (get-random-passive-values 10) (get-random-promoter-values 70)))))
+           (is (= 70 (get-promoter-score (->> [] (get-random-detractor-values 100) (get-random-passive-values 100) (get-random-promoter-values 800)))))
+           (is (= -70 (get-promoter-score (->> [] (get-random-detractor-values 8000) (get-random-passive-values 1000) (get-random-promoter-values 1000)))))
            (is (= -40 (get-promoter-score [1 2 3 4 5 6 7 8 9 10]))))
 
   (testing "Correctly calculates the NPS when provided custom min/max values"
