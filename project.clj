3ea1c64be3c37d6978c4556e2d3283e999c02f57
Eric Boersma <eric.boersma@gmail.com>
2019-01-09 14:04:55 -0500

    Updating project.clj with information for publishing

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject net_promoter_score "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject net_promoter_score "1.0.0"
+  :description "A pure clojure library for calculating net promoter score values."
+  :url "https://github.com/EricBoersma/net_promoter_score"
+  :license {:name "MIT license"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [
                   [org.clojure/clojure "1.9.0"]
                   [clojure.java-time "0.3.2"]
